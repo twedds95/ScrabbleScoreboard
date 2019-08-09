1043ed688f0ebddcd54dd80c48a1d367c54b4890
twedd <twedd@DESKTOP-PHHSBET>
2019-08-08 22:14:37 -0400

    Basic appearance is done. Must add functionality to components and icons.

--- src/scrabblescoreboard/Scoreboard.java
+++ src/scrabblescoreboard/Scoreboard.java
@@ -7,16 +7,19 @@ package scrabblescoreboard;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.SpringLayout;
 
 /**
@@ -33,6 +36,8 @@ public class Scoreboard extends JFrame implements ActionListener {
 
     Font Fscrabble = new Font("gothic standard", Font.BOLD, 24);
 
+    Cursor hoverCursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
+
     JPanel JPplayers = new JPanel(),
             JPword = new JPanel(),
             JPoptions = new JPanel();
@@ -45,31 +50,35 @@ public class Scoreboard extends JFrame implements ActionListener {
             JTname2 = new JTextField("Player 2", 15),
             JTword = new JTextField("Enter word", 15);
 
-    JLabel JLscore = new JLabel(),
-            JLscore1 = new JLabel("0"),
-            JLscore2 = new JLabel("0");
+    JLabel JLscore = new JLabel("", JLabel.CENTER),
+            JLscore1 = new JLabel("0", JLabel.CENTER),
+            JLscore2 = new JLabel("0", JLabel.CENTER),
+            JLmultiplier = new JLabel("", JLabel.CENTER);
 
-    JButton[] JBtiles = new JButton[15],
-            JBbonus = new JButton[4];
+    JButton[] JBtiles = new JButton[15];
 
-    private int score1 = 0;
-    private int score2 = 0;
-    private int turn = 0;
+    JButton JBdoubleWord = new JButton("2x"),
+            JBtripleWord = new JButton("3x"),
+            JBbonusUndo = new JButton("Undo"),
+            JBendTurn = new JButton("End Turn"),
+            JBaddWord = new JButton("Add Another Word");
 
-    public void paint(Graphics g) {
-        g.setColor(Color.gray);
-        g.fillRect(1, 1, getWidth(), getHeight());
-    }
+    JToggleButton JTBdoubleLetter = new JToggleButton("2L"),
+            JTBtripleLetter = new JToggleButton("3L");
+
+    ButtonGroup BGletterBonus = new ButtonGroup();
+
+    private int score1 = 0,
+            score2 = 0,
+            playedScore = 0,
+            turn = 0;
+    
+    private String playedWord;
 
     public Scoreboard(int width, int height) {
-        //Create frame
-        setBounds(100, 100, width, height);
-        setTitle("Scrabble Scoreboard");
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
         //Set layout for panels
+        setLayout(new GridLayout(0, 1));
         JPplayers.setLayout(playersLayout);
         JPword.setLayout(wordLayout);
         JPoptions.setLayout(optionsLayout);
@@ -84,20 +93,21 @@ public class Scoreboard extends JFrame implements ActionListener {
         JTname2.setBackground(Color.blue);
         JTname1.setHorizontalAlignment(JTextField.CENTER);
         JTname2.setHorizontalAlignment(JTextField.CENTER);
-        JTname1.setSize(100, 40);
-        JTname2.setSize(100, 40);
+
         playersLayout.putConstraint(SpringLayout.WEST, JTname1,
-                width / 3,
-                SpringLayout.WEST, getContentPane());
-        playersLayout.putConstraint(SpringLayout.EAST, JTname2,
-                width / 3,
-                SpringLayout.EAST, getContentPane());
+                width / 5,
+                SpringLayout.WEST, JPplayers);
+        playersLayout.putConstraint(SpringLayout.WEST, JTname2,
+                2 * width / 5 - 200,
+                SpringLayout.EAST, JTname1);
         playersLayout.putConstraint(SpringLayout.NORTH, JTname1,
                 10,
-                SpringLayout.NORTH, getContentPane());
+                SpringLayout.NORTH, JPplayers);
         playersLayout.putConstraint(SpringLayout.NORTH, JTname2,
                 10,
-                SpringLayout.NORTH, getContentPane());
+                SpringLayout.NORTH, JPplayers);
+        JTname1.setPreferredSize(new Dimension(100, 40));
+        JTname2.setPreferredSize(new Dimension(100, 40));
         JTname1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -115,73 +125,216 @@ public class Scoreboard extends JFrame implements ActionListener {
         //initialize scores on panel
         JLscore1.setBackground(Color.white);
         JLscore2.setBackground(Color.white);
+        JLscore1.setForeground(Color.black);
+        JLscore2.setForeground(Color.black);
         JLscore1.setFont(Fscrabble);
         JLscore2.setFont(Fscrabble);
         JLscore1.setBorder(BorderFactory.createLineBorder(Color.red, 3));
         JLscore2.setBorder(BorderFactory.createLineBorder(Color.blue, 3));
-        JLscore1.setHorizontalAlignment(JLabel.CENTER);
-        JLscore2.setHorizontalAlignment(JLabel.CENTER);
-        playersLayout.putConstraint(SpringLayout.WEST, JLscore1,
-                width / 3,
-                SpringLayout.WEST, getContentPane());
-        playersLayout.putConstraint(SpringLayout.EAST, JLscore2,
-                width / 3,
-                SpringLayout.EAST, getContentPane());
+        JLscore1.setPreferredSize(new Dimension(50, 50));
+        JLscore2.setPreferredSize(new Dimension(50, 50));
+        JLscore1.setOpaque(true);
+        JLscore2.setOpaque(true);
+        playersLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JLscore1,
+                0,
+                SpringLayout.HORIZONTAL_CENTER, JTname1);
+        playersLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JLscore2,
+                0,
+                SpringLayout.HORIZONTAL_CENTER, JTname2);
         playersLayout.putConstraint(SpringLayout.NORTH, JLscore1,
                 10,
                 SpringLayout.SOUTH, JTname1);
-        playersLayout.putConstraint(SpringLayout.NORTH, JLscore1,
+        playersLayout.putConstraint(SpringLayout.NORTH, JLscore2,
                 10,
                 SpringLayout.SOUTH, JTname2);
 
         JTword.setBackground(Color.red);
         JTword.setHorizontalAlignment(JTextField.CENTER);
-        wordLayout.putConstraint(SpringLayout.NORTH, JTword,
-                10,
-                SpringLayout.NORTH, JPword);
-        JTword.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String pword = JTword.getText();
-                int pscore = calculateScore(pword, JBtiles);
-                JTword.setText("Modify Word");
+        JTword.setPreferredSize(new Dimension(100, 40));
+        playersLayout.putConstraint(SpringLayout.NORTH, JTword,
+                25,
+                SpringLayout.SOUTH, JLscore1);
+        playersLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JTword,
+                width / 2,
+                SpringLayout.WEST, JPplayers);
+        //Setup JPword panel, so only need to set visible when Word is entered
+        JLscore.setBackground(Color.white);
+        JLscore.setFont(Fscrabble);
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JLscore, width / 2 - 75, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JLscore, 135, SpringLayout.NORTH, JPword);
+        JLscore.setBorder(BorderFactory.createLineBorder(Color.black, 3));
+        JLscore.setForeground(Color.black);
+        JLscore.setPreferredSize(new Dimension(50, 50));
+        JLscore.setVisible(false);
 
-            }
-        });
-        // for action listener of Confirm button
-//          turn++;
-//        if (turn % 2 == 1) {
-//                    word.setBackground(Color.blue);
-//                    score1 += calculateScore(pword);
-//
-//                } else {
-//                    word.setBackground(Color.red);
-//                    score2 += calculateScore(pword);
-//                }
+        JTBdoubleLetter.setPreferredSize(new Dimension(50, 50));
+        JTBtripleLetter.setPreferredSize(new Dimension(50, 50));
+        JBdoubleWord.setPreferredSize(new Dimension(50, 50));
+        JBtripleWord.setPreferredSize(new Dimension(50, 50));
+        JBbonusUndo.setPreferredSize(new Dimension(75, 50));
+        JBaddWord.setPreferredSize(new Dimension(150, 50));
+        JBendTurn.setPreferredSize(new Dimension(150, 50));
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JTBdoubleLetter, width / 6, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JTBtripleLetter, width * 2 / 6, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JBdoubleWord, width * 3 / 6, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JBtripleWord, width * 4 / 6, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, JBbonusUndo, width * 5 / 6, SpringLayout.WEST, JPword);
+        wordLayout.putConstraint(SpringLayout.WEST, JBendTurn, 10, SpringLayout.EAST, JLscore);
+        wordLayout.putConstraint(SpringLayout.WEST, JBaddWord, 10, SpringLayout.EAST, JBendTurn);
+        wordLayout.putConstraint(SpringLayout.WEST, JBendTurn, 10, SpringLayout.EAST, JLscore);
+        wordLayout.putConstraint(SpringLayout.NORTH, JTBdoubleLetter, 10, SpringLayout.NORTH, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JTBtripleLetter, 10, SpringLayout.NORTH, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JBdoubleWord, 10, SpringLayout.NORTH, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JBtripleWord, 10, SpringLayout.NORTH, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JBbonusUndo, 10, SpringLayout.NORTH, JPword);
+        wordLayout.putConstraint(SpringLayout.NORTH, JBaddWord, 0, SpringLayout.NORTH, JLscore);
+        wordLayout.putConstraint(SpringLayout.NORTH, JBendTurn, 0, SpringLayout.NORTH, JLscore);
+        JTBdoubleLetter.setCursor(hoverCursor);
+        JTBtripleLetter.setCursor(hoverCursor);
+        JBdoubleWord.setCursor(hoverCursor);
+        JBdoubleWord.setCursor(hoverCursor);
+        JBbonusUndo.setCursor(hoverCursor);
+        JBaddWord.setCursor(hoverCursor);
+        JBendTurn.setCursor(hoverCursor);
+        JTBdoubleLetter.setVisible(false);
+        JTBtripleLetter.setVisible(false);
+        JBdoubleWord.setVisible(false);
+        JBtripleWord.setVisible(false);
+        JBbonusUndo.setVisible(false);
+        JBaddWord.setVisible(false);
+        JBendTurn.setVisible(false);
+        BGletterBonus.add(JTBdoubleLetter);
+        BGletterBonus.add(JTBtripleLetter);
 
         JPplayers.add(JTname1);
         JPplayers.add(JTname2);
         JPplayers.add(JLscore1);
         JPplayers.add(JLscore2);
-        add(JPplayers);
-        JPplayers.setVisible(true);
+        JPplayers.add(JTword);
+
+        JPword.add(JLscore);
+        JPword.add(JTBdoubleLetter);
+        JPword.add(JTBtripleLetter);
+        JPword.add(JBdoubleWord);
+        JPword.add(JBtripleWord);
+        JPword.add(JBbonusUndo);
+        JPword.add(JBendTurn);
+        JPword.add(JBaddWord);
+        
+        getContentPane().add(JPplayers);
+        getContentPane().add(JPword);
+        getContentPane().add(JPoptions);
+        // Action Listeners for Various Cmmponents
+
+        JBaddWord.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (turn % 2 == 0) {
+                    JTword.setBackground(Color.blue);
+                    score1 += playedScore;
+                    JLscore1.setText(Integer.toString(score1));
+
+                } else {
+                    JTword.setBackground(Color.red);
+                    score2 += playedScore;
+                    JLscore2.setText(Integer.toString(score2));
+                }
+                playedScore = 0;
+                JTBdoubleLetter.setVisible(false);
+                JTBtripleLetter.setVisible(false);
+                JBdoubleWord.setVisible(false);
+                JBtripleWord.setVisible(false);
+                JBbonusUndo.setVisible(false);
+                JBaddWord.setVisible(false);
+                JBendTurn.setVisible(false);
+                JLscore.setVisible(false);
+                JTword.setText("Enter Word");
+                for (int i = 0; i < playedWord.length(); i++) {
+                    JBtiles[i].setVisible(false);
+                }
+
+            }
+        }
+        );
+        
+        JBendTurn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                turn++;
+                if (turn % 2 == 1) {
+                    JTword.setBackground(Color.blue);
+                    score1 += playedScore;
+                    JLscore1.setText(Integer.toString(score1));
+
+                } else {
+                    JTword.setBackground(Color.red);
+                    score2 += playedScore;
+                    JLscore2.setText(Integer.toString(score2));
+                }
+                playedScore = 0;
+                JTBdoubleLetter.setVisible(false);
+                JTBtripleLetter.setVisible(false);
+                JBdoubleWord.setVisible(false);
+                JBtripleWord.setVisible(false);
+                JBbonusUndo.setVisible(false);
+                JBaddWord.setVisible(false);
+                JBendTurn.setVisible(false);
+                JLscore.setVisible(false);
+                JTword.setText("Enter Word");
+                for (int i = 0; i < playedWord.length(); i++) {
+                    JBtiles[i].setVisible(false);
+                }
+
+            }
+        }
+        );
+
+        JTword.addActionListener(
+                new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e
+            ) {
+                playedWord = JTword.getText();
+                playedScore = calculateScore(playedWord, JBtiles);
+                JTword.setText("Modify Word");
+            }
+        }
+        );
+
     }
 
+    /**
+     *
+     * @param word
+     * @param JBtile
+     * @return
+     */
     public int calculateScore(String word, JButton[] JBtile) {
         int score = 0;
+
+        JTBdoubleLetter.setVisible(true);
+        JTBtripleLetter.setVisible(true);
+        JBdoubleWord.setVisible(true);
+        JBtripleWord.setVisible(true);
+        JBbonusUndo.setVisible(true);
+        JBendTurn.setVisible(true);
+        JBaddWord.setVisible(true);
+
         int i = 0;
         for (char letter : word.toCharArray()) {
             JBtile[i].setText(String.valueOf(Character.toUpperCase(letter)));
             JBtile[i].setBackground(new Color(255, 255, 153));
             JBtile[i].setHorizontalAlignment(JButton.CENTER);
             JBtile[i].setFont(Fscrabble);
-            wordLayout.putConstraint(SpringLayout.NORTH, JBtile[i], 10, SpringLayout.SOUTH, JTword);
-            JBtile[i].setBounds(getWidth() / 2 - 25 * word.length() + 50 * i, getHeight() / 3 + 100, 50, 50);
+            JPword.add(JBtile[i]);
+            wordLayout.putConstraint(SpringLayout.NORTH, JBtile[i], 70, SpringLayout.NORTH, JPword);
+            wordLayout.putConstraint(SpringLayout.WEST, JBtile[i],
+                    (getWidth() / 2 - 25 * word.length() + 50 * i),
+                    SpringLayout.WEST, JPword);
             JBtile[i].setBorder(BorderFactory.createLineBorder(Color.gray, 2));
             JBtile[i].setForeground(Color.black);
-            JBtile[i].setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-            JBtile[i].setVisible(true);
-            add(JBtile[i]);
+            JBtile[i].setCursor(hoverCursor);
+            JBtile[i].setPreferredSize(new Dimension(50, 50));
             JBtile[i].setVisible(true);
             score += getLetterScore(letter);
             i++;
@@ -191,13 +344,7 @@ public class Scoreboard extends JFrame implements ActionListener {
         }
 
         JLscore.setText(Integer.toString(score));
-        JLscore.setBackground(Color.white);
-        JLscore.setHorizontalAlignment(JTextField.CENTER);
-        JLscore.setFont(Fscrabble);
-        JLscore.setBounds(getWidth() / 2 - 25, getHeight() / 3 + 200, 50, 50);
-        JLscore.setBorder(BorderFactory.createLineBorder(Color.black, 3));
-        JLscore.setForeground(Color.black);
-        add(JLscore);
+        JLscore.setVisible(true);
         return score;
     }
 
@@ -206,9 +353,12 @@ public class Scoreboard extends JFrame implements ActionListener {
         return LETTER_SCORES[upperCase - 'A'];
     }
 
+    /**
+     *
+     * @param arg0
+     */
     @Override
     public void actionPerformed(ActionEvent arg0) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
